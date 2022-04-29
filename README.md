b8a116d67d0ed79e10539f51a79dfd970f9cbce4
Dienga John Divine <70081718+John-Divine@users.noreply.github.com>
2022-04-29 12:45:32 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Handwritten-equation-solver---CSC498-PROJECT
+ This project is for my end of year CSC program
---
+++ handwritten_equation_solver/HE_solver/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HeSolverConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'HE_solver'
