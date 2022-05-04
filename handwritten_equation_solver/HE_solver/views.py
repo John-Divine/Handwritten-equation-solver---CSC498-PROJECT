208e9133217a98587b14c8c30497041ecdc7b299
Dienga John Divine <70081718+John-Divine@users.noreply.github.com>
2022-05-04 16:01:52 -0700

    sketch pad layout

--- handwritten_equation_solver/HE_solver/views.py
+++ handwritten_equation_solver/HE_solver/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
+from django.core import serializers
 
 # Create your views here.
 def index(request):
     return render(request,'HE_solver/index.html')
+
+def predict(request):
+    if request.is_ajax and request.method =='POST':
+
+
